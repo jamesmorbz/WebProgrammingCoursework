9a407519a99b3181ebecf1bac509510e17c8e73a
James Morbz <jamesmoreby1@gmail.com>
2023-11-14 15:10:52 +0000

    Improving Models + Fixing Choice Fields

--- api/models.py
+++ api/models.py
@@ -2,40 +2,40 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
-class CATEGORIES(models.TextChoices): #Enum class of news catergories
+class Subjects(models.TextChoices):
       SPORT = "Sport"
       FINANCE = "Finance"
       POLITICS = "Politics"
       HEALTH = "Health"
 
+class Categories(models.Model):
+    category = models.TextField(choices=Subjects.choices)
+
 class User(AbstractUser):
-    EMAIL_FIELD = models.EmailField("Email", editable=True)
-    USERNAME_FIELD = models.CharField("Username", max_length=200, primary_key=True)
-    password = models.CharField("Password", max_length=200) #This may have to change when authentication is implemented
+    password = models.CharField("password", max_length=32)
+    first_name = models.CharField("first_name", max_length=20, editable=True)
+    last_name = models.CharField("surname", max_length=20, editable=True)
+    date_of_birth = models.DateField("DOB", editable=True, null=True, blank=True)
+    profile_picture = models.ImageField("profile_picture", upload_to="uploads", editable=True, null=True, blank=True)
+    favourite_categories = models.ManyToManyField(Categories, verbose_name="favourites", editable=True)
 
-    first_name = models.CharField("Name", max_length=200)
-    last_name = models.CharField("Surname", max_length=200)
-    date_of_birth = models.DateField("D.O.B.", editable=True)
-    profile_picture = models.ImageField("Profile Picture", upload_to="uploads", editable=True)
-    favourite_categories = models.ManyToManyField("Favourite Categories", choices=CATEGORIES) #Trying to find a way to make this field and multi select
-
-    REQUIRED_FIELDS = ["EMAIL_FIELD", "USERNAME_FIELD", "password", "date_of_birth", "profile_picture"]
+    REQUIRED_FIELDS = ["password"]
 
 class Article(models.Model):
-    article_id = models.AutoField("Article ID", primary_key=True)
-    author = models.CharField("Author", max_length=200)
-    category = models.CharField(choices=CATEGORIES, max_length=8)
-    date_time_posted = models.DateTimeField("Date/Time Posted")
-    date_time_edited = models.DateTimeField("Date/Time Edited", default=date_time_posted) #Should update whenever an edit to an article is saved
-    contents = models.TextField("Contents", editable=True)
+    article_id = models.AutoField("article_id", primary_key=True)
+    author = models.CharField("author", max_length=200)
+    category = models.TextField(choices=Subjects.choices)
+    date_time_posted = models.DateTimeField("time_posted")
+    date_time_edited = models.DateTimeField("time_edited", default=date_time_posted) #Should update whenever an edit to an article is saved
+    contents = models.TextField("content", editable=True)
 
 class Comment(models.Model):
-    comment_id = models.AutoField("Comment ID", primary_key=True)
-    posted_by = models.ForeignKey("User", on_delete=models.SET_NULL)
-    date_time_posted = models.DateTimeField("Date/Time Posted")
-    date_time_edited = models.DateTimeField("Date/Time Edited", default=date_time_posted) #Should update whenever an edit to a comment is saved
-    contents = models.TextField("Contents", editable=True)
+    comment_id = models.AutoField("comment_id", primary_key=True)
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+    date_time_posted = models.DateTimeField("time_posted")
+    date_time_edited = models.DateTimeField("time_edited", default=date_time_posted) #Should update whenever an edit to a comment is saved
+    contents = models.TextField("content", editable=True)
 
 class CommentSection(models.Model): #One article should be able to have many comments
-    article = models.ForeignKey(Article)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
     comments = models.ManyToManyField(Comment)
