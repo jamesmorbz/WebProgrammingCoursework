3ebfe66d6fb0decd0352d6afdf7b11f08a8eef72
James Morbz <jamesmoreby1@gmail.com>
2023-11-16 22:40:42 +0000

    Adding Filtering Logic to Blog Posts

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -2,10 +2,28 @@
   <div class="main-blog-page">
     <div class="header">
       <h1>{{ title }}</h1>
+      <div class="category-buttons">
+        <button @click="toggleUserFavorites" :class="{ 'selected': showUserFavorites }">
+          User Favorites
+        </button>
+        <button
+          v-for="category in uniqueCategories"
+          :key="category"
+          @click="toggleCategoryFilter(category)"
+          :class="{ 'selected': isCategoryFiltered(category) }"
+        >
+          {{ category }}
+        </button>
+        
+      </div>
     </div>
 
-    <router-link v-for="article in articles" :key="article.id" :to="{ name: 'Article', params: { id: article.id } }"
-      class="blog-article-link">
+    <router-link
+      v-for="article in filteredArticles"
+      :key="article.id"
+      :to="{ name: 'Article', params: { id: article.id }}"
+      class="blog-article-link"
+    >
       <BlogArticle :article="article" />
     </router-link>
   </div>
@@ -19,6 +37,8 @@ export default defineComponent({
   data() {
     return {
       title: "Main Page",
+      selectedCategories: [] as string[], // Selected categories for filtering
+      showUserFavorites: true, // Flag to show user favorites
       articles: [
         {
           id: 1,
@@ -45,10 +65,46 @@ export default defineComponent({
           date: "2023-11-14",
         },
       ],
+      userFavorites: ["Technology", "Health"], // Example user favorites
     };
+  },
+  computed: {
+    uniqueCategories() {
+      return Array.from(new Set(this.articles.map(article => article.category))); // Extract unique category values from articles
+    },
+    filteredArticles() {
+      let articles = this.articles.filter(article => this.selectedCategories.includes(article.category));
+      if (this.showUserFavorites) {
+        let favourites = this.articles.filter(article => this.userFavorites.includes(article.category)); // Show articles that match user favorites
+        return Array.from(new Set(favourites.concat(articles)));
+      }
+
+      if (this.selectedCategories.length === 0) {
+        return this.articles; // No categories selected, show all articles
+      }
+
+      return articles;
+    },
   },
   components: {
     BlogArticle,
+  },
+  methods: {
+    toggleCategoryFilter(category: string) {
+      const index = this.selectedCategories.indexOf(category);
+
+      if (index === -1) {
+        this.selectedCategories.push(category);
+      } else {
+        this.selectedCategories.splice(index, 1);
+      }
+    },
+    isCategoryFiltered(category: string) {
+      return this.selectedCategories.includes(category);
+    },
+    toggleUserFavorites() {
+      this.showUserFavorites = !this.showUserFavorites;
+    },
   },
 });
 </script>
@@ -63,6 +119,30 @@ export default defineComponent({
 .header {
   text-align: center;
   margin-bottom: 20px;
+}
+
+.category-buttons {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+}
+
+.category-buttons button {
+  margin: 0 5px;
+  padding: 8px;
+  cursor: pointer;
+  border: none;
+  border-radius: 20px;
+  transition: background-color 0.3s ease;
+}
+
+.category-buttons button:hover {
+  background-color: #f0f0f0;
+}
+
+.category-buttons button.selected {
+  background-color: #007BFF;
+  color: #fff;
 }
 
 .blog-article-link {
