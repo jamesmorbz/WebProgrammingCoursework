6160330e6fc3a365977dd9dd8978da41c1c3fb6b
ZainabH20 <zainab.habib@kpmg.co.uk>
2023-11-19 12:59:17 +0000

    profile page form validation and ajax requests

--- frontend/src/components/CustomCheckbox.vue
+++ frontend/src/components/CustomCheckbox.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="form-check">
-    <input
-      class="form-check-input"
-      type="checkbox"
-      :id="category"
-      :value="category"
-      :checked="isChecked"
-    />
+    <input class="form-check-input" type="checkbox" :id="category" :value="category" :checked="isChecked"
+      @click="$emit('updateFavourites', category)" />
     <label class="form-check-label" :for="category">{{ category }}</label>
   </div>
 </template>
--- frontend/src/pages/ProfilePage.vue
+++ frontend/src/pages/ProfilePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex-shrink-0">
     <div id="header">
-      <div class="h3">Profile Page</div>
+      <div class="h3">{{ title }}</div>
       <p id="smallText">Date Joined: {{ userData.dateJoined }}</p>
     </div>
     <div id="body">
@@ -9,46 +9,18 @@
         <div class="row g-3 mb-3">
           <div class="col-md-2">
             <div class="d-flex align-items-start">
-              <img
-                v-if="formData.updatedAvatar"
-                :src="formData.updatedAvatar"
-                class="rounded"
-                alt="Preview of uploaded image"
-                id="profile-image-prev"
-              />
-              <img
-                v-else
-                :src="
-                  userData.profileAvatar
-                    ? userData.profileAvatar
-                    : defaultAvatar
-                "
-                class="rounded"
-                alt="Default Image"
-                id="profile-image-prev"
-              />
+              <img v-if="formData.updatedAvatar" :src="formData.updatedAvatar" class="rounded"
+                alt="Preview of uploaded image" id="profile-image-prev" />
+              <img v-else :src="formData.updatedAvatar" class="rounded" alt="Default Image" id="profile-image-prev" />
             </div>
           </div>
         </div>
         <div class="row g-3 mb-3">
           <div class="col-md-12">
-            <label for="profile-edit-image" class="form-label"
-              >Profile Image</label
-            >
+            <label for="profile-edit-image" class="form-label">Profile Image</label>
             <div class="input-group">
-              <input
-                class="form-control"
-                type="file"
-                id="formFile"
-                accept="image/*"
-                @change="handleFileChange"
-              />
-              <button
-                class="btn btn-outline-secondary"
-                type="button"
-                id="button-remove"
-                @click="removeUpdatedImage"
-              >
+              <input class="form-control" type="file" id="formFile" accept="image/*" @change="handleFileChange" />
+              <button class="btn btn-outline-secondary" type="button" id="button-remove" @click="removeUpdatedImage">
                 Remove
               </button>
             </div>
@@ -56,59 +28,33 @@
         </div>
         <div class="row g-3 mb-3">
           <div class="col-md-6">
-            <label for="profile-edit-username" class="form-label"
-              >Username</label
-            >
-            <input
-              type="text"
-              class="form-control"
-              id="profile-edit-username"
-              :value="userData.username"
-            />
+            <label for="profile-edit-username" class="form-label">Username</label>
+            <input type="text" class="form-control" id="profile-edit-username" v-model="formData.username"
+              placeholder="Please enter a username" required :class="{ 'is-invalid': !isUsernameValid }" />
+            <div v-if="!isUsernameValid" class="invalid-feedback">Invalid username</div>
           </div>
           <div class="col-md-6">
-            <label for="profile-edit-full-name" class="form-label"
-              >Full Name</label
-            >
-            <input
-              type="text"
-              class="form-control"
-              id="profile-edit-full-name"
-              :value="fullName"
-              disabled
-            />
+            <label for="profile-edit-full-name" class="form-label">Full Name</label>
+            <input type="text" class="form-control" id="profile-edit-full-name" v-model="fullName" disabled />
           </div>
         </div>
         <div class="row g-3 mb-3">
           <div class="col-md-6">
             <label for="profile-edit-email" class="form-label">Email</label>
-            <input
-              type="email"
-              class="form-control"
-              id="profile-edit-email"
-              placeholder="Please enter your email address"
-              :value="userData.email"
-            />
+            <input type="email" class="form-control" id="profile-edit-email" placeholder="Please enter your email address"
+              v-model="formData.email" required :class="{ 'is-invalid': !isEmailValid }" />
+            <div v-if="!isEmailValid" class="invalid-feedback">Invalid email address</div>
           </div>
           <div class="col-md-6">
-            <label for="profile-edit-dob" class="form-label"
-              >Date of Birth</label
-            >
-            <input
-              type="date"
-              class="form-control"
-              id="profile-edit-dob"
-              :value="formattedDOB"
-            />
+            <label for="profile-edit-dob" class="form-label">Date of Birth</label>
+            <input type="date" class="form-control" id="profile-edit-dob" v-model="formData.dob" />
           </div>
         </div>
         <div class="mb-3">
           <label for="profile-edit-dob" class="form-label">Categories</label>
-          <div v-for="item in categories" :key="item">
-            <CustomCheckbox
-              :category="item"
-              :isChecked="userData.categories.includes(item)"
-            />
+          <div v-for="(item, index) in categories" :key="item">
+            <CustomCheckbox :category="item" :isChecked="formData.categories.includes(item)"
+              @checkbox="updateFavourites(index)" />
           </div>
         </div>
       </form>
@@ -118,6 +64,7 @@
     </div>
   </div>
 </template>
+
 
 <script lang="ts">
 import { defineComponent } from 'vue'
@@ -132,23 +79,25 @@ export default defineComponent({
   data() {
     return {
       title: 'Profile Page',
+      isUsernameValid: true,
+      isEmailValid: true,
+      userData: {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'temp@hotmail.com',
+        dob: '2002-12-23',
+        dateJoined: '2023-01-23',
+        username: 'John1',
+        categories: ['Finance', 'Sport', 'Health'],
+        //logic for loading image from backend ??
+        profileAvatar: testImage,
+      },
       formData: {
         username: '',
         email: '',
         dob: '',
         categories: [] as string[],
         updatedAvatar: '',
-      },
-      userData: {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'temp@hotmail.com',
-        dob: '2002/12/23',
-        dateJoined: '2023/01/23',
-        username: 'John1',
-        categories: ['Finance', 'Sport', 'Health'],
-        //logic for loading image from backend ??
-        profileAvatar: testImage,
       },
       categories: ['Finance', 'Politics', 'Sport', 'Health'],
       defaultAvatar: defaultAvatar,
@@ -158,13 +107,19 @@ export default defineComponent({
     fullName() {
       return `${this.userData.firstName} ${this.userData.lastName}`
     },
-    formattedDOB() {
-      // Convert date to to "YYYY-MM-DD" format ?? depends how stored in db
-      const [year, month, day] = this.userData.dob.split('/')
-      return `${year}-${month}-${day}`
-    },
+    // formattedDOB() {
+    //   // Convert date to to "YYYY-MM-DD" format ?? depends how stored in db
+    //   const [year, month, day] = this.userData.dob.split('/')
+    //   return `${year}-${month}-${day}`
+    // },
   },
-  mounted() {},
+  mounted() {
+    this.formData.username = this.userData.username
+    this.formData.email = this.userData.email
+    this.formData.dob = this.userData.dob
+    this.formData.categories = this.userData.categories
+    this.formData.updatedAvatar = this.userData.profileAvatar ? this.userData.profileAvatar : defaultAvatar
+  },
   methods: {
     handleFileChange(event: Event) {
       const input = event.target as HTMLInputElement
@@ -184,11 +139,75 @@ export default defineComponent({
     removeUpdatedImage(event: Event) {
       const input = event.target as HTMLInputElement
       input.value = ''
-      this.formData.updatedAvatar = ''
+      this.formData.updatedAvatar = defaultAvatar
     },
-    submitForm() {
+    updateFavourites(index: number) {
+      const category = this.categories[index];
+
+      // Check if the category is already in formData.categories
+      const categoryIndex = this.formData.categories.indexOf(category);
+
+      if (categoryIndex !== -1) {
+        // If found, remove it
+        this.formData.categories.splice(categoryIndex, 1);
+      } else {
+        // If not found, add it
+        this.formData.categories.push(category);
+      }
+    },
+    async getUserData() {
+      try {
+        const response = await fetch('getuserprofileurl', {
+          method: 'GET',
+        });
+        if (response.ok) {
+          const data = await response.json();
+          this.userData = data
+        }
+      } catch (error) {
+        console.log('Error fetching user profile', error);
+      }
+    },
+    async submitForm() {
       // Handle form submission, you can send the formData to the server
-      console.log('Form submitted:', this.formData, this.userData)
+      if (this.isFormValid()) {
+        // Handle form submission
+        const response = await fetch('updateuserprofileurl', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            body: JSON.stringify(this.formData)
+          }
+        });
+        if (response.ok) {
+          this.userData = {
+            ...this.userData,
+            username: this.formData.username,
+            email: this.formData.email,
+            dob: this.formData.dob,
+            categories: this.formData.categories,
+            profileAvatar: this.formData.updatedAvatar,
+          };
+          console.log('Form submitted:', this.formData);
+        }
+        else {
+          console.log('Something wrong with submitting form:', response.status, response.statusText);
+        }
+      } else {
+        console.log('Form validation failed');
+      }
+    },
+    isFormValid() {
+      // Add additional validation conditions as needed
+      this.isUsernameValid = this.formData.username.length >= 2;
+      this.isEmailValid = this.isValidEmail(this.formData.email);
+      // Return true if all validations pass
+      return this.isUsernameValid && this.isEmailValid;
+    },
+    isValidEmail(email: string) {
+      // Add your email validation logic here
+      const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+      return emailRegex.test(email);
     },
   },
 })
